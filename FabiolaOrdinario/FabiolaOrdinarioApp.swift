9932d0bfe6042609c2a28c7e279a0e76a03f98ed
Fabiola <fabiola.juarez>
2025-12-01 17:42:37 -0600

    Initial Commit

---
+++ FabiolaOrdinario/FabiolaOrdinarioApp.swift
@@ -0,0 +1,17 @@
+//
+//  FabiolaOrdinarioApp.swift
+//  FabiolaOrdinario
+//
+//  Created by win603 on 01/12/25.
+//
+
+import SwiftUI
+
+@main
+struct FabiolaOrdinarioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
